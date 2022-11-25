bb78ff79f8cf639b8510dcbd66b3a6808587b2fc
Oli Sturm <oliver@oliversturm.com>
2022-11-25 17:17:25 +0000

    updated for .NET 4.8, VS 2022

--- Mandelbrot/AutomaticConcurrent/Form1.cs
+++ Mandelbrot/AutomaticConcurrent/Form1.cs
@@ -1,5 +1,5 @@
 ﻿#region Copyright
-/* Copyright 2009 Oliver Sturm <oliver@sturmnet.org> All rights reserved. */
+/* Copyright 2009-2010 Oliver Sturm <oliver@sturmnet.org> All rights reserved. */
 #endregion
 
 using System;
@@ -12,91 +12,105 @@ using System.Windows.Forms;
 using FunctionalMandelbrotParallel;
 using System.Collections.Generic;
 using System.Threading;
-using System.Threading.Collections;
+using System.Collections.Concurrent;
 using System.Threading.Tasks;
 
-namespace AutomaticConcurrent {
-  public partial class Form1 : Form {
-    public Form1( ) {
-      InitializeComponent( );
+namespace AutomaticConcurrent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
 
-      syncContext = SynchronizationContext.Current;
-    }
-
-    SynchronizationContext syncContext;
-
-    const double xstart = -2.1;
-    const double xend = 1.0;
-    const double ystart = -1.3;
-    const double yend = 1.3;
-
-    private void DrawButton_Click(object sender, EventArgs e) {
-      int height = panel.Height;
-      int width = panel.Width;
-      paintImage = new Bitmap(width, height);
-      int iterations = (int) IterationsUD.Value;
-      Calculator.MaxIteration = iterations;
-      double xstep = (xend-xstart)/width;
-      double ystep = (yend-ystart)/height;
-
-      resultQueue = new ConcurrentQueue<PointResult>( );
-      Task uiUpdateTask = Task.Create(o => UIUpdate( ));
-
-      var calcInfo = new CalcInfo(xstart, xstep, ystart, ystep);
-      Task mainTask = Task.Create(o =>
-        Calculator.CalcArea(panel.Width, panel.Height, calcInfo, AcceptResult));
-      mainTask.ContinueWith(o =>
-        uiUpdateTask.CancelAndWait( ));
-    }
-
-    const int UPDATE_THRESHOLD = 10000;
-
-    private void UIUpdateFromResults(int count) {
-      Point maxPoint = new Point(0, 0), minPoint = new Point(Int32.MaxValue, Int32.MaxValue);
-      lock(paintImageLock) {
-        for (int i = 0; i < count; i++) {
-          PointResult pointResult;
-
-          if (resultQueue.TryDequeue(out pointResult)) {
-            paintImage.SetPixel(pointResult.Point.X, pointResult.Point.Y, pointResult.Color);
-            minPoint = new Point(Math.Min(minPoint.X, pointResult.Point.X), Math.Min(minPoint.Y, pointResult.Point.Y));
-            maxPoint = new Point(Math.Max(maxPoint.X, pointResult.Point.X), Math.Max(maxPoint.Y, pointResult.Point.Y));
-          }
+            syncContext = SynchronizationContext.Current;
         }
-      }
-      syncContext.Post(o => {
-        panel.Invalidate(new Rectangle(minPoint, new Size(maxPoint.X - minPoint.X, maxPoint.Y - minPoint.Y)));
-      }, null);
-    }
 
-    void UIUpdate( ) {
-      while (!(Task.Current.IsCanceled)) {
-        if (resultQueue.Count > UPDATE_THRESHOLD)
-          UIUpdateFromResults(UPDATE_THRESHOLD);
-        else
-          Thread.Sleep(50);
-      }
-      UIUpdateFromResults(resultQueue.Count);
-    }
+        SynchronizationContext syncContext;
 
-    void AcceptResult(PointResult pointResult) {
-      resultQueue.Enqueue(pointResult);
-    }
+        const double xstart = -2.1;
+        const double xend = 1.0;
+        const double ystart = -1.3;
+        const double yend = 1.3;
 
-    ConcurrentQueue<PointResult> resultQueue;
-    Bitmap paintImage;
-    object paintImageLock = new object( );
+        private void DrawButton_Click(object sender, EventArgs e)
+        {
+            Calculator.MaxIteration = Convert.ToInt32(IterationsUD.Value);
 
-    private void ClearButton_Click(object sender, EventArgs e) {
-      paintImage = null;
-      panel.Invalidate( );
-    }
+            int height = panel.Height;
+            int width = panel.Width;
+            paintImage = new Bitmap(width, height);
+            double xstep = (xend - xstart) / width;
+            double ystep = (yend - ystart) / height;
 
-    private void panel_Paint(object sender, PaintEventArgs e) {
-      if (paintImage != null) {
-        lock(paintImageLock)
-          e.Graphics.DrawImage(paintImage, 0, 0);
-      }
+            resultQueue = new ConcurrentQueue<PointResult>();
+            var uiCancel = new CancellationTokenSource();
+            Task uiUpdateTask = Task.Factory.StartNew(() => UIUpdate(uiCancel.Token), uiCancel.Token);
+
+            var calcInfo = new CalcInfo(xstart, xstep, ystart, ystep);
+            Task mainTask = Task.Factory.StartNew(() =>
+              Calculator.CalcArea(panel.Width, panel.Height, calcInfo, AcceptResult));
+            mainTask.ContinueWith(t => uiCancel.Cancel());
+        }
+
+        const int UPDATE_THRESHOLD = 10000;
+
+        private void UIUpdateFromResults(int count)
+        {
+            Point maxPoint = new Point(0, 0), minPoint = new Point(Int32.MaxValue, Int32.MaxValue);
+            lock (paintImageLock)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    PointResult pointResult;
+
+                    if (resultQueue.TryDequeue(out pointResult))
+                    {
+                        paintImage.SetPixel(pointResult.Point.X, pointResult.Point.Y, pointResult.Color);
+                        minPoint = new Point(Math.Min(minPoint.X, pointResult.Point.X), Math.Min(minPoint.Y, pointResult.Point.Y));
+                        maxPoint = new Point(Math.Max(maxPoint.X, pointResult.Point.X), Math.Max(maxPoint.Y, pointResult.Point.Y));
+                    }
+                }
+            }
+            syncContext.Post(o => {
+                panel.Invalidate(new Rectangle(minPoint, new Size(maxPoint.X - minPoint.X, maxPoint.Y - minPoint.Y)));
+            }, null);
+        }
+
+        void UIUpdate(CancellationToken token)
+        {
+            while (!(token.IsCancellationRequested))
+            {
+                if (resultQueue.Count > UPDATE_THRESHOLD)
+                    UIUpdateFromResults(UPDATE_THRESHOLD);
+                else
+                    Thread.Sleep(50);
+            }
+            UIUpdateFromResults(resultQueue.Count);
+        }
+
+        void AcceptResult(PointResult pointResult)
+        {
+            resultQueue.Enqueue(pointResult);
+        }
+
+        ConcurrentQueue<PointResult> resultQueue;
+        Bitmap paintImage;
+        object paintImageLock = new object();
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            paintImage = null;
+            panel.Invalidate();
+        }
+
+        private void panel_Paint(object sender, PaintEventArgs e)
+        {
+            if (paintImage != null)
+            {
+                lock (paintImageLock)
+                    e.Graphics.DrawImage(paintImage, 0, 0);
+            }
+        }
     }
-  }
 }
--- Mandelbrot/FunctionalMandelbrotParallel/Calculator.cs
+++ Mandelbrot/FunctionalMandelbrotParallel/Calculator.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Drawing;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace FunctionalMandelbrotParallel {
   public static class Calculator {
